ea02bed04e2f5ac238652e21239326962d8f131c
Robert W. Leonard <robert@draknor.com>
2019-04-11 20:12:58 -0500

    Add aiming ability for player and AI

--- BattleTank/Source/BattleTank/Public/TankAIController.h
+++ BattleTank/Source/BattleTank/Public/TankAIController.h
@@ -15,7 +15,11 @@ class BATTLETANK_API ATankAIController : public AAIController
 {
 	GENERATED_BODY()
 	
+public:
+
+private:
 	virtual void BeginPlay() override;
+	virtual void Tick(float DeltaTime) override;
 
 	ATank* GetControlledTank() const;
 	ATank* GetPlayerTank() const;
